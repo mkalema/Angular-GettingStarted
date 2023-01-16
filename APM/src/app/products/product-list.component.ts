03bdd06ccb1434c0c2f6eaeb38670042cc1ae686
Moses Kalema <mkgremo20@gmail.com>
2023-01-15 22:30:08 -0600

    filtering and arrow functions

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -21,7 +21,10 @@ export class ProductListComponent implements OnInit{
 
   set listFilter(value: string){
     this._listFilter = value;
+    this.filteredProducts = this.performFilter(value);
   }
+
+  filteredProducts: IProduct[] = [];
 
   products:IProduct[] = [
     {
@@ -82,5 +85,11 @@ export class ProductListComponent implements OnInit{
 
   ngOnInit(): void {
     this._listFilter = 'cart';
+  }
+
+  performFilter(filterBy: string): IProduct[]{
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.products.filter((product: IProduct) =>
+      product.productName.toLocaleLowerCase().includes(filterBy));
   }
 }
